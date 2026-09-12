504d35a52f3d3b0997e3cdfb586ae1b30ef82e39
hemantschavan77-cmyk <hemantschavan77@gmail.com>
2026-09-12 14:06:33 +0530

    Externalize CORS allowed origin via application.properties and CorsConfig

--- src/main/java/com/recipehub/backend/controller/RecipeController.java
+++ src/main/java/com/recipehub/backend/controller/RecipeController.java
@@ -2,7 +2,6 @@ package com.recipehub.backend.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,7 +17,6 @@ import com.recipehub.backend.service.RecipeService;
 
 @RestController
 @RequestMapping("/api/recipes")
-@CrossOrigin(origins = "http://localhost:3000")
 public class RecipeController {
 
    private final RecipeService recipeService;
