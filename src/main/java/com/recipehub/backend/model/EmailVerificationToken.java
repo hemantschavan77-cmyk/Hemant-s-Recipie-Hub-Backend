f41620af3c177fdb01c44af967a70a895f505918
hemantschavan77-cmyk <hemantschavan77@gmail.com>
2026-09-20 13:08:03 +0530

    Added EmailVerificationToken model for email varification

---
+++ src/main/java/com/recipehub/backend/model/EmailVerificationToken.java
@@ -0,0 +1,66 @@
+package com.recipehub.backend.model;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "email_verification_tokens")
+public class EmailVerificationToken {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private Long id;
+	
+	private String token;
+	
+	private LocalDateTime expiresAt;
+	
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public EmailVerificationToken()
+	{
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public LocalDateTime getExpiresAt() {
+		return expiresAt;
+	}
+
+	public void setExpiresAt(LocalDateTime expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
